7a875e03fefdc417920c5cdaf3493e440b30723c
maxiwis <330939@uwr.edu.pl>
2022-02-06 03:28:37 +0100

    added main

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include "engine.h"
 
+#include <math.h>
+#include <SDL2/SDL_events.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_keycode.h>
 #include <SDL2/SDL_rect.h>
+#include <SDL2/SDL_render.h>
 #include <SDL2/SDL_surface.h>
 #include <SDL2/SDL_timer.h>
-#include <math.h>
+#include <SDL2/SDL_video.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,4 +20,25 @@ const int noPipes = 6;
 
 int main(int argc, char **argv) {
     srand(time(NULL));
+
+    Engine silnikGry;
+    if(!initGame(&silnikGry)) {
+        printf("failed to init!\n");
+    }
+    else {
+        if(!loadMedia(&silnikGry))
+            printf("failed to init!\n");
+        else {
+            SDL_Event imprezka; /// :D
+
+            while(silnikGry.state != QUIT_GAME) {
+                input(&silnikGry, &imprezka);
+
+                updateGame(&silnikGry);
+
+                renderFrame(&silnikGry);
+            }
+        }
+    }
+    closeGame(&silnikGry);
 }
